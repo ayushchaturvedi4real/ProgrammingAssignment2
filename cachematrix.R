2faee730f76c3228d06930fb5943d604495128d4
Ayush Chaturvedi <chaturvedia336@gmail.com>
2019-03-31 19:55:19 +0530

    This is my updated cachematrix code

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,78 @@
 ## functions do
 
 ## Write a short comment describing this function
+#using function set to set the matrix to m
+#using function get to return the matrix
+#using function setinv to set the inverse matrix to inverse_matrix
+#using function getinv to return the inverse matrix
+#return a list of functions
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  
+  i<- NULL
+  
+  #use function set to set the matrix to m
+  
+  set<-function( matrix){
+    x<<-matrix
+    i<<- NULL
+    
+  }
+  
+  #use function get to return the matrix
+  
+  get<-function() {
+    x
+  }
+  
+  #use function setinv to set the inverse matrix to i
+  
+  setinv<-function(inverse){
+    i<<-inverse
+  }
+  
+  #use function getinv to return the inverse matrix
+  
+  getinv<-function(){
+    i
+  }
+  
+  #returning a list of functions
+  list(set = set, get = get , setinv = setinv , getinv = getinv)
+  
 }
 
 
 ## Write a short comment describing this function
+#set the inverse to m_inv
+#return inverse if set otherwise compute it
+#if m_inv is null then calculate the inverse of the matrix
+#set the inverse matrix
+#return the inverse matrix
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  #set the inverse to m_inv 
+  i <-x$getinv()
+  
+  
+  #return inverse if set otherwise compute it
+  if(!is.null(i)){
+    message("get cache data")
+    return(i)
+  }
+  #if m_inv is null then calculate the inverse of the matrix
+  matrix<-x$get()
+  #calculatng its inverse
+  i<-solve(matrix)
+  #set the inverse
+  x$setinv(i)
+  #return the inverse
+  i
+  
+  
 }
